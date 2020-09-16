e2fbe03a07a4c18add2d05c6131b4f0114b90b2e
Samuel Granato <samuelgranato87@gmail.com>
2020-09-16 20:37:25 -0300

    clean master

--- SDC1.sdc
+++
@@ -1,5 +0,0 @@
-# 50MHz board input clock
-create_clock -period 20 [get_ports fpga_clk_50]
-
-# Automatically apply a generate clock on the output of phase-locked loops (PLLs) 
-derive_pll_clocks
--- niosLab2/niosLab2_inst.v
+++
@@ -1,9 +0,0 @@
-	niosLab2 u0 (
-		.clk_clk              (<connected-to-clk_clk>),              //           clk.clk
-		.pio_0_leds_export    (<connected-to-pio_0_leds_export>),    //    pio_0_leds.export
-		.pio_1_switchs_export (<connected-to-pio_1_switchs_export>), // pio_1_switchs.export
-		.pio_2_button_export  (<connected-to-pio_2_button_export>),  //  pio_2_button.export
-		.pio_3_motor_export   (<connected-to-pio_3_motor_export>),   //   pio_3_motor.export
-		.reset_reset_n        (<connected-to-reset_reset_n>)         //         reset.reset_n
-	);
-
